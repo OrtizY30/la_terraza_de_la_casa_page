950a8f0b3d1f8045cb1e5d7ffd0ecf94e33bb1dc
Ysreimer Ortiz <ysreimer@gmail.com>
2024-06-07 23:22:31 -0500

    Actualizacion 07-06-2024

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,31 +6,38 @@ import useFirestore from "../hooks/useFirestore";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const [fav, setFav] = useState(JSON.parse(localStorage.getItem("favoritos")) || []);
+  const [fav, setFav] = useState(
+    JSON.parse(localStorage.getItem("favoritos")) || []
+  );
   // localStorage.removeItem('menu')
 
   const togleFav = (product) => {
     // Obtener los productos favoritos del localStorage
-  const favFromLocalStorage = JSON.parse(localStorage.getItem("favoritos")) || [];
+    const favFromLocalStorage =
+      JSON.parse(localStorage.getItem("favoritos")) || [];
 
-  // Verificar si el producto ya está en favoritos
-  const index = favFromLocalStorage.findIndex((item) => item.id === product.id);
+    // Verificar si el producto ya está en favoritos
+    const index = favFromLocalStorage.findIndex(
+      (item) => item.id === product.id
+    );
 
-  // Si no está en favoritos, añadirlo
-  if (index === -1) {
-    const updatedFav = [...favFromLocalStorage, product];
-    setFav(updatedFav);
+    // Si no está en favoritos, añadirlo
+    if (index === -1) {
+      const updatedFav = [...favFromLocalStorage, product];
+      setFav(updatedFav);
 
-    // Guardar los favoritos actualizados en el localStorage
-    localStorage.setItem("favoritos", JSON.stringify(updatedFav));
-  } else {
-    // Si está en favoritos, eliminarlo
-    const updatedFav = favFromLocalStorage.filter((item) => item.id !== product.id);
-    setFav(updatedFav);
-    // Guardar los favoritos actualizados en el localStorage
-    localStorage.setItem("favoritos", JSON.stringify(updatedFav));
-  }
-}
+      // Guardar los favoritos actualizados en el localStorage
+      localStorage.setItem("favoritos", JSON.stringify(updatedFav));
+    } else {
+      // Si está en favoritos, eliminarlo
+      const updatedFav = favFromLocalStorage.filter(
+        (item) => item.id !== product.id
+      );
+      setFav(updatedFav);
+      // Guardar los favoritos actualizados en el localStorage
+      localStorage.setItem("favoritos", JSON.stringify(updatedFav));
+    }
+  };
 
   useEffect(() => {
     localStorage.setItem("favoritos", JSON.stringify(fav));
