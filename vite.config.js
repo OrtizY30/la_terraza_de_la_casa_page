48de5bacc9424b3dd652e3cee82aba12e624a009
Ysreimer Ortiz <ysreimer@gmail.com>
2024-10-14 02:55:52 -0500

    Actualizacion a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'La Terraza De La Casa',
-        short_name: 'La Terraza De La Casa',
-        description: 'Aplicación del restaurante para realizar pedidos online.',
-        theme_color: 'transparent',
-        background_color: '#FFFFFF',
+        name: "La Terraza De La Casa",
+        short_name: "La Terraza De La Casa",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        scope: "/",
+        description: "Aplicación del restaurante para realizar pedidos online.",
+        theme_color: "transparent",
         icons: [
           {
-            src: 'assets/logocasual.avif',
-            sizes: '192x192',
-            type: 'image/avif'
+            src: "assets/logocasual.avif",
+            sizes: "192x192",
+            type: "image/avif",
           },
           {
-            src: 'assets/logocasual.avif',
-            sizes: '512x512',
-            type: 'image/avif'
-          }
-        ]
-      }
-    })
+            src: "assets/logocasual.avif",
+            sizes: "512x512",
+            type: "image/avif",
+          },
+        ],
+      },
+    }),
   ],
   build: {
     rollupOptions: {
